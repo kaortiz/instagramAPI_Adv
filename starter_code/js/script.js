032f09f409b7b727c6ac603322dd281d2fb5ea21
kaortiz <kaortiz@gmail.com>
2016-02-04 13:50:44 +0000

    Flickr Project KO'

--- starter_code/js/script.js
+++ starter_code/js/script.js
@@ -1,6 +1,19 @@
 // YOUR CODE GOES HERE
-var clientID = "REPLACE ME WITH CLIENT ID";
+var clientID = "36aa5c6b3029e11d9b4daeea8da6276e";
 
 $(document).ready(function() {
     // your code here
+    $("#search-btn").click(function() {
+        var userSelection = $("#search-query").val();
+        searchFlickr(clientID, userSelection, 10, function(photos) {
+            for (i = 0; i < 10; i++) {
+                $("#search-results").append("<img src=" + photos.photo[i].url_m + ">");
+            }
+            console.log(photos.photo[i]);
+            $("#reset-btn").click(function(){
+                $("#search-results").empty()
+            })
+        })
+    });
+
 });
